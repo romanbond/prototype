7e61bdfde8d17bd6d529d0a6d1a4371245985400
romanbond <romanjbond@gmail.com>
2016-11-14 11:47:57 -0500

    Added Data Sets

--- DataTest.playground/Contents.swift
+++ DataTest.playground/Contents.swift
@@ -2,4 +2,17 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var TSLA = [
+    "9/26/16": 20899,
+    "9/27/16": 20581,
+    "9/28/16": 20627,
+    "9/29/16": 20070,
+    "9/30/16": 20403,
+    "10/3/16": 21370,
+    "10/4/16": 21141,
+    "10/5/16": 20846,
+    "10/6/16": 20100,
+    "10/7/16": 19661
+]
+
+//Add other symbols, try to pull from firebase or something.
